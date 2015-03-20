bf407a9e8c8a737e89aa78cdb014bc4feb1e37f8
Petr Potnin <potninp@gmail.com>
2015-03-20 20:59:08 +0300

    First implementation
    Seems to work. But it's still unclear how this works and, moreover, how
    to use it in real life

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,43 @@
 ## These two functions are here to test assgning objects 
-## in a different environment.
+## in a different environment in order to cache some
+## computationally-expensive values
 
-## First one constructsts a matrix
+## First one constructsts a "matrix" object
+## that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## Initialize the inverse matrix
+    inv <- NULL
+    ## Set matrix
+    set <- function (input) {
+        matr <<- input
+        inv <<- NULL
+    }
+    ## Get the matrix
+    get <- function() matr
+    ## Set the inverse
+    setinv <- function (inverse) inv <<- inverse
+    ## Get the inverse value
+    getinv <- function () inv
+    ## Return a list of functions
+    list (set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## This one calculates the inverse or takes it from cache
-## (if available)
+## The second  one calculates the inverse or takes it
+## from cache (if available)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Get inverse, see if it actually exitsts
+    inv <- x$getinv()
+    if (!is.null(inv)) {
+        ## If inverse exists, get it
+        message("getting cache data, hooray!")
+        return (inv)
+    }
+    ## If inverse doesn't exist, calculate it
+    data <- x$get()
+    inv <- solve (data, ...)
+    x$setinv(inv)
+    inv
 }

8ea534bb5c6612c71244ebd55a3e71585e97887c
Petr Potnin <potninp@gmail.com>
2015-03-20 19:58:02 +0300

    Some comments
    Raw comments, mostly there to experiment on committing etc.

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions are here to test assgning objects 
+## in a different environment.
 
-## Write a short comment describing this function
+## First one constructsts a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This one calculates the inverse or takes it from cache
+## (if available)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
